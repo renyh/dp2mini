5c0c8a17c9689ecc17e4d4f1de86bcbeb32ed0a5
yang wu <wuyang@dp2003.com>
2021-06-03 18:50:54 +0800

    更改“来源系统”代码

--- dp2mini/LoginForm.designer.cs
+++ dp2mini/LoginForm.designer.cs
@@ -46,10 +46,9 @@ namespace dp2mini
             // 
             // button_cancel
             // 
-            this.button_cancel.Location = new System.Drawing.Point(557, 231);
-            this.button_cancel.Margin = new System.Windows.Forms.Padding(4);
+            this.button_cancel.Location = new System.Drawing.Point(371, 154);
             this.button_cancel.Name = "button_cancel";
-            this.button_cancel.Size = new System.Drawing.Size(112, 34);
+            this.button_cancel.Size = new System.Drawing.Size(75, 23);
             this.button_cancel.TabIndex = 8;
             this.button_cancel.Text = "取消(&C)";
             this.button_cancel.UseVisualStyleBackColor = true;
@@ -57,10 +56,9 @@ namespace dp2mini
             // 
             // button_login
             // 
-            this.button_login.Location = new System.Drawing.Point(437, 231);
-            this.button_login.Margin = new System.Windows.Forms.Padding(4);
+            this.button_login.Location = new System.Drawing.Point(291, 154);
             this.button_login.Name = "button_login";
-            this.button_login.Size = new System.Drawing.Size(112, 34);
+            this.button_login.Size = new System.Drawing.Size(75, 23);
             this.button_login.TabIndex = 7;
             this.button_login.Text = "登录";
             this.button_login.UseVisualStyleBackColor = true;
@@ -68,76 +66,68 @@ namespace dp2mini
             // 
             // textBox_password
             // 
-            this.textBox_password.Location = new System.Drawing.Point(147, 122);
-            this.textBox_password.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_password.Location = new System.Drawing.Point(98, 81);
             this.textBox_password.Name = "textBox_password";
             this.textBox_password.PasswordChar = '*';
-            this.textBox_password.Size = new System.Drawing.Size(286, 28);
+            this.textBox_password.Size = new System.Drawing.Size(192, 21);
             this.textBox_password.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(22, 126);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(15, 84);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 18);
+            this.label3.Size = new System.Drawing.Size(41, 12);
             this.label3.TabIndex = 4;
             this.label3.Text = "密码：";
             // 
             // textBox_username
             // 
-            this.textBox_username.Location = new System.Drawing.Point(147, 81);
-            this.textBox_username.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_username.Location = new System.Drawing.Point(98, 54);
             this.textBox_username.Name = "textBox_username";
-            this.textBox_username.Size = new System.Drawing.Size(286, 28);
+            this.textBox_username.Size = new System.Drawing.Size(192, 21);
             this.textBox_username.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 81);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(15, 54);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 18);
+            this.label2.Size = new System.Drawing.Size(53, 12);
             this.label2.TabIndex = 2;
             this.label2.Text = "用户名：";
             // 
             // textBox_libraryUrl
             // 
-            this.textBox_libraryUrl.Location = new System.Drawing.Point(147, 40);
-            this.textBox_libraryUrl.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_libraryUrl.Location = new System.Drawing.Point(98, 27);
             this.textBox_libraryUrl.Name = "textBox_libraryUrl";
-            this.textBox_libraryUrl.Size = new System.Drawing.Size(544, 28);
+            this.textBox_libraryUrl.Size = new System.Drawing.Size(364, 21);
             this.textBox_libraryUrl.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 45);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(15, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(116, 18);
+            this.label1.Size = new System.Drawing.Size(77, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "服务器地址：";
             // 
             // checkBox_savePassword
             // 
             this.checkBox_savePassword.AutoSize = true;
-            this.checkBox_savePassword.Location = new System.Drawing.Point(147, 158);
-            this.checkBox_savePassword.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox_savePassword.Location = new System.Drawing.Point(98, 105);
             this.checkBox_savePassword.Name = "checkBox_savePassword";
-            this.checkBox_savePassword.Size = new System.Drawing.Size(106, 22);
+            this.checkBox_savePassword.Size = new System.Drawing.Size(72, 16);
             this.checkBox_savePassword.TabIndex = 6;
             this.checkBox_savePassword.Text = "记住密码";
             this.checkBox_savePassword.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 285);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(9, 190);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(169, 34);
+            this.button1.Size = new System.Drawing.Size(113, 23);
             this.button1.TabIndex = 9;
             this.button1.Text = "打开数据文件夹";
             this.button1.UseVisualStyleBackColor = true;
@@ -145,9 +135,9 @@ namespace dp2mini
             // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(717, 332);
+            this.ClientSize = new System.Drawing.Size(505, 225);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox_savePassword);
             this.Controls.Add(this.button_cancel);
@@ -158,7 +148,6 @@ namespace dp2mini
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox_libraryUrl);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "LoginForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
